7aa38a363eaea592ce42cb74111a372b623ed60a
Adobyte <adobyte77@gmail.com>
2023-12-17 19:20:10 +0100

    Add function to hold process
    -Also 'if __name__' statement
    -Correct common_password() to common_password in line 5

--- common_password_checker.py
+++ common_password_checker.py
@@ -2,7 +2,16 @@ def check_password(password):
     with open('passwords.text', 'r') as file:
         common_passwords = file.read().splitlines()
         for i, common_password in enumerate(common_passwords, start=1):
-            if password == common_password():
+            if password == common_password:
                 print(f'❌  (#{i})')
                 return
         print('✅  (Unique)')
+
+
+def main_func():
+    user_pass = input("Enter password: ")
+    check_password(user_pass)
+
+
+if __name__ == '__main__':
+    main_func()
